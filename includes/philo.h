93f982963d68194e98819b9dcfcc72eb0aa40233
eezn <pplaneoo@gmail.com>
2021-12-15 00:13:49 +0900

    Add(init_data): Set input data and Initialize data

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 20:35:09 by jin-lee           #+#    #+#             */
-/*   Updated: 2021/12/06 09:23:22 by jin-lee          ###   ########.fr       */
+/*   Updated: 2021/12/15 00:02:40 by jin-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,22 @@
 # include <pthread.h>
 # include "mini_libft.h"
 # include "err_message.h"
+
+# define TRUE 1
+# define FALSE 0
+
+typedef struct s_data	t_data;
+
+struct s_data
+{
+	int				philosophers;
+	int				time_to_die;
+	int				time_to_eat;
+	int 			time_to_sleep;
+	int				must_eat_flag;
+	int				must_eat;
+	struct timeval	g_timestamp;
+};
 
 /* utils */
 void	pass(void);
--- srcs/main.c
+++ srcs/main.c
@@ -6,38 +6,41 @@
 /*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 20:14:44 by jin-lee           #+#    #+#             */
-/*   Updated: 2021/12/06 09:29:44 by jin-lee          ###   ########.fr       */
+/*   Updated: 2021/12/15 00:01:29 by jin-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-/**
- * @brief 
- * 
- * @param argc 
- * @param argv
- * argv[0]
- * argv[1]: number_of_philosophers (do not over 200)
- * argv[2]: time_to_die (do not under 60ms)
- * argv[3]: time_to_eat (do not under 60ms)
- * argv[4]: time_to_sleep (do not under 60ms)
- * argv[5]: (optional) number_of_times_each_philosopher_must_eat
- * @return int 
- */
+void	init_data(int argc, char **argv, t_data *data)
+{
+	data->philosophers = ft_atoi(argv[1]);
+	data->time_to_die = ft_atoi(argv[2]);
+	data->time_to_eat = ft_atoi(argv[3]);
+	data->time_to_sleep = ft_atoi(argv[4]);
+	data->must_eat_flag = FALSE;
+	if (argc == 6)
+	{
+		data->must_eat_flag = TRUE;
+		data->must_eat = ft_atoi(argv[5]);
+	}
+	gettimeofday(&data->g_timestamp, NULL);
+}
+
+/* ************************************************************************** */
 
 int	main(int argc, char **argv)
 {
-	if (argc == 5 || argc == 6)
-	{
-		if (check_args(argc, argv))
-			return (EXIT_FAILURE);
-		if (argc == 5)
-			pass();
-		if (argc == 6)
-			pass();
-	}
-	else
+	t_data	data;
+	
+	if (!(argc == 5 || argc == 6))
 		print_error(ERR_ARGS);
-	return (EXIT_SUCCESS);
+
+	if (check_args(argc, argv))
+		return (EXIT_FAILURE);
+	else
+	{
+		init_data(argc, argv, &data);
+		return (EXIT_SUCCESS);
+	}
 }
