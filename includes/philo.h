4fdf9d2381734662037d6e5b7fdfa989c8896aa9
eezn <pplaneoo@gmail.com>
2021-12-15 10:46:41 +0900

    temp(philo_list): Temporary save

--- includes/philo.h
+++ includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/05 20:35:09 by jin-lee           #+#    #+#             */
-/*   Updated: 2021/12/15 00:02:40 by jin-lee          ###   ########.fr       */
+/*   Updated: 2021/12/15 10:45:41 by jin-lee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,41 @@
 # define TRUE 1
 # define FALSE 0
 
-typedef struct s_data	t_data;
+/* ************************************************************************** */
 
-struct s_data
+typedef struct	s_philo_list	t_philo_list;
+
+struct	s_philo_list
+{
+	t_philo			*head;
+	t_philo			*tail;
+	size_t			count;
+};
+
+/* ************************************************************************** */
+
+typedef struct	s_data			t_data;
+typedef struct	s_philo			t_philo;
+typedef struct	s_chopstick t_chopstick;
+
+struct	s_chopstick
+{
+	int				curr_chopstick;
+	
+	pthread_mutex_t	*mutex_lock;
+};
+
+struct	s_philo
+{
+	int				curr_philo;
+
+	pthread_t		thread;
+	t_chopstick		chopstick;
+	t_philo			*next;
+	t_philo			*prev;
+};
+
+struct	s_data
 {
 	int				philosophers;
 	int				time_to_die;
@@ -35,8 +67,13 @@ struct s_data
 	int 			time_to_sleep;
 	int				must_eat_flag;
 	int				must_eat;
+
 	struct timeval	g_timestamp;
+	
+	t_philo			*arr_philo;
 };
+
+/* ************************************************************************** */
 
 /* utils */
 void	pass(void);
